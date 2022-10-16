a90d47a23669db8f01fa0fe08b1ccd393bb6299d
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-10-16 21:44:27 +0200

    Change: update dependencies for develop-branch

--- build.sh
+++ build.sh
@@ -49,49 +49,22 @@ function get_required_kitsune_lib_repo () {
     build_kitsune_lib_repo $REPO_NAME $NUMBER_OF_THREADS $ADDITIONAL_CONFIGS
 }
 
-function get_required_private_repo_github () {
-    REPO_NAME=$1
-    TAG_OR_BRANCH=$2
-    NUMBER_OF_THREADS=$3
-
-    # clone repo
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
-    cd "$PARENT_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-
-    build_kitsune_lib_repo $REPO_NAME $NUMBER_OF_THREADS
-}
-
-function download_private_repo_github () {
-    REPO_NAME=$1
-    TAG_OR_BRANCH=$2
-
-    # clone repo
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$BUILD_DIR/$REPO_NAME"
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
-    cd "$BUILD_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-    cd "$PARENT_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-}
-
-
 #-----------------------------------------------------------------------------------------------------------------
 
-get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.27.0" 8
-get_required_kitsune_lib_repo "libKitsunemimiJson" "develop" 1
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_github "libKitsunemimiSqlite" "develop" 8
+get_required_kitsune_lib_repo "libKitsunemimiCommon" "develop" 8 "staticlib"
+get_required_kitsune_lib_repo "libKitsunemimiJson" "develop" 1 "staticlib"
+get_required_kitsune_lib_repo "libKitsunemimiSqlite" "develop" 8 "staticlib"
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_github "libKitsunemimiSakuraDatabase" "develop" 8
+get_required_kitsune_lib_repo "libKitsunemimiSakuraDatabase" "develop" 8 "staticlib"
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_github "libKitsunemimiHanamiCommon" "develop" 8
+get_required_kitsune_lib_repo "libKitsunemimiHanamiCommon" "develop" 8 "staticlib"
 
 #-----------------------------------------------------------------------------------------------------------------
 
